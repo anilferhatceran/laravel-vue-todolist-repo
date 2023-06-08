ec70e4a6470293f443142634df7b6cc1d887bd2a
Anil Ferhat Ceran <69785410+anilferhatceran@users.noreply.github.com>
2023-06-08 14:29:00 +0200

    Cleanup

--- laravel-vue-todolist/resources/js/components/LoginView.vue
+++ laravel-vue-todolist/resources/js/components/LoginView.vue
@@ -1,10 +1,18 @@
 <template>
     <div class="flex justify-center mt-6">
-        <form @submit.prevent="submitLogin" class="w-full max-w-lg bg-gray-50 border-2 border-gray-100 py-8 rounded px-8">
-            <h1 class="font-semibold text-2xl text-center mb-6 uppercase">Login</h1>
+        <form
+            @submit.prevent="submitLogin"
+            class="w-full max-w-lg bg-gray-50 border-2 border-gray-100 py-8 rounded px-8"
+        >
+            <h1 class="font-semibold text-2xl text-center mb-6 uppercase">
+                Login
+            </h1>
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
-                    <label class="block uppercase text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="email">
+                    <label
+                        class="block uppercase text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                        for="email"
+                    >
                         Email
                     </label>
                 </div>
@@ -16,12 +24,15 @@
                         type="email"
                         placeholder="Your email.."
                         required
-                    >
+                    />
                 </div>
             </div>
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
-                    <label class="block uppercase text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="password">
+                    <label
+                        class="block uppercase text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                        for="password"
+                    >
                         Password
                     </label>
                 </div>
@@ -33,11 +44,14 @@
                         type="password"
                         placeholder="Your password.."
                         required
-                    >
+                    />
                 </div>
             </div>
             <div class="flex justify-end">
-                <button type="submit" class="shadow uppercase bg-blue-500 hover:bg-blue-600 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
+                <button
+                    type="submit"
+                    class="shadow uppercase bg-blue-500 hover:bg-blue-600 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                >
                     Login
                 </button>
             </div>
@@ -46,39 +60,31 @@
 </template>
 
 <script setup>
-import axios from "axios";
-
-axios.defaults.withCredentials = true
-
-
-import {reactive, ref} from "vue";
+import { reactive, ref } from "vue";
 import { useRouter } from "vue-router";
+
+import axios from "axios";
+axios.defaults.withCredentials = true;
 
 const router = useRouter();
 
-
 const user = reactive({
-    'email':'',
-    'password': ''
+    email: "",
+    password: "",
 });
 
-const submitLogin = async() => {
+const submitLogin = async () => {
     try {
-        await axios.get('/sanctum/csrf-cookie');
-        await axios.post('/login', user);
-        const response = await axios.get('/api/user');
+        await axios.get("/sanctum/csrf-cookie");
+        await axios.post("/login", user);
+        const response = await axios.get("/api/user");
 
-        user.value = response.data
-        return router.push('/').then(() => {
+        user.value = response.data;
+        return router.push("/").then(() => {
             router.go();
         });
-
-    }catch (error){
+    } catch (error) {
         console.error(error);
     }
-
-
-}
-
-
+};
 </script>
