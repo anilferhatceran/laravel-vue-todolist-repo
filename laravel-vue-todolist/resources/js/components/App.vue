abc879a4da2e511ad2b7d6cb23ae5aea0c7f6b25
Anil Ferhat Ceran <69785410+anilferhatceran@users.noreply.github.com>
2023-06-29 17:07:06 +0200

    Changed home button text

--- laravel-vue-todolist/resources/js/components/App.vue
+++ laravel-vue-todolist/resources/js/components/App.vue
@@ -1,48 +1,58 @@
 <template>
     <nav class="flex items-center justify-between flex-wrap bg-white p-6">
         <div class="flex items-center flex-shrink-0 mr-6">
-            <span class="font-semibold text-2xl tracking-tight mr-2"><RouterLink to="/">wecode</RouterLink></span>
-            <RouterLink to="/" class="mx-2 text-gray-700 hover:text-black">Home</RouterLink>
-            <RouterLink to="/todos/create" class="ml-2 text-gray-700 hover:text-black">Add Todo</RouterLink>
+            <span class="font-semibold text-2xl tracking-tight mr-2"
+                ><RouterLink to="/">Todo List</RouterLink></span
+            >
+            <RouterLink to="/" class="mx-2 text-gray-700 hover:text-black"
+                >Home</RouterLink
+            >
+            <RouterLink
+                to="/todos/create"
+                class="ml-2 text-gray-700 hover:text-black"
+                >Add Todo</RouterLink
+            >
         </div>
 
         <div v-if="currentUser">
-            <span class="font-semibold text-gray-600">{{currentUser && currentUser.name}}</span>
+            <span class="font-semibold text-gray-600">{{
+                currentUser && currentUser.name
+            }}</span>
         </div>
 
         <div>
-            <RouterLink to="/register" class="text-gray-700 hover:text-black">{{currentUser ? 'Register another account' : 'Register'}}</RouterLink>
-            <RouterLink to="/login" class="ml-2 text-gray-700 hover:text-black">{{currentUser ? 'Change account' : 'Login'}}</RouterLink>
+            <RouterLink to="/register" class="text-gray-700 hover:text-black">{{
+                currentUser ? "Register another account" : "Register"
+            }}</RouterLink>
+            <RouterLink
+                to="/login"
+                class="ml-2 text-gray-700 hover:text-black"
+                >{{ currentUser ? "Change account" : "Login" }}</RouterLink
+            >
         </div>
     </nav>
     <div class="p-6">
         <RouterView></RouterView>
     </div>
-
 </template>
 <script setup>
 import axios from "axios";
-import {onMounted, onUpdated, ref} from "vue";
+import { onMounted, onUpdated, ref } from "vue";
 axios.defaults.withCredentials = true;
 
 let refreshKey = false;
 
 const currentUser = ref();
 
-onMounted(async() => {
+onMounted(async () => {
     try {
-            await axios.get('/sanctum/csrf-token');
-            const response = await axios.get('/api/user');
+        await axios.get("/sanctum/csrf-token");
+        const response = await axios.get("/api/user");
 
-            refreshKey = true;
-            return currentUser.value = response.data;
-
-    }catch (error){
+        refreshKey = true;
+        return (currentUser.value = response.data);
+    } catch (error) {
         console.error(error);
-
     }
-})
-
-
-
+});
 </script>
